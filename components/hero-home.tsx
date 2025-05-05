9772dee1e13e769945cacba71ca870a462ca6d88
no-inspi <71600397+no-inspi@users.noreply.github.com>
2025-05-05 14:31:02 +0200

    fix all redirect link to doc

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -47,7 +47,7 @@ export default function HeroHome() {
                 <div data-aos="fade-up" data-aos-delay={400}>
                   <a
                     className="btn group mb-4 w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="/documentation/quickstart"
+                    href="https://doc.melify.io/docs/intro"
                   >
                     <span className="relative inline-flex items-center">
                       Get Started
